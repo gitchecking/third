cb82489ffc1fe403e45a8986b3143974d5840b8b
rsrp94 <47897085+rsrp94@users.noreply.github.com>
2019-07-26 13:54:04 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testing",
+    version="0.1",
+    packages=["testing"],
+    url="https://github.com/gitchecking/third",
+    install_requires=[
+        "basescript==0.3.0",
+        "PyGithub==1.43.5",
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
